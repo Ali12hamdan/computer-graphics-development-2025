786268b474514f6fde1f75f9a6878f99bc942d3a
Ali Hamdan <alihamdan@Alis-MacBook-Air-2.local>
2025-03-16 01:44:55 +0300

    TODO 2.01 test-1

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -10,6 +10,27 @@ void cg::renderer::ray_tracing_renderer::init()
 	// TODO Lab: 2.01 Add `render_target`, `camera`, and `raytracer` in `ray_tracing_renderer` class
 	// TODO Lab: 2.03 Add light information to `lights` array of `ray_tracing_renderer`
 	// TODO Lab: 2.04 Initialize `shadow_raytracer` in `ray_tracing_renderer`
+
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+		settings->width, settings->height);
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+	camera = std::make_shared<cg::world::camera>();
+	camera->set_height(static_cast<float>(settings->height));
+	camera->set_width(static_cast<float>(settings->width));
+	camera->set_position(float3{
+		settings->camera_position[0],
+		settings->camera_position[1],
+		settings->camera_position[2]
+	});
+	camera->set_theta(settings->camera_theta);
+	camera->set_phi(settings->camera_phi);
+	camera->set_angle_of_view(settings->camera_angle_of_view);
+	camera->set_z_near(settings->camera_z_near);
+	camera->set_z_far(settings->camera_z_far);
+	raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+	raytracer->set_viewport(settings->width, settings->height);
+	raytracer->set_render_target(render_target);
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -25,4 +46,22 @@ void cg::renderer::ray_tracing_renderer::render()
 	// TODO Lab: 2.04 Adjust `closest_hit_shader` of `raytracer` to cast shadows rays and to ignore occluded lights
 	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
 	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
+
+	raytracer->miss_shader = [](const ray& ray){
+		payload payload{};
+		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
+		return  payload;
+	};
+	raytracer->clear_render_target({0,0,0,});
+	auto start = std::chrono::high_resolution_clock::now();
+	raytracer->ray_generation(
+			camera->get_position(), camera->get_direction(),
+			camera->get_right(), camera->get_up(),
+			settings->raytracing_depth, settings->accumulation_num);
+	auto stop = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float, std::milli> rt_duration = stop-start;
+	std::cout<<"Ray tracing took "<< rt_duration.count()<<"ms\n";
+	cg::utils::save_resource(*render_target, settings->result_path);
+
+
 }
